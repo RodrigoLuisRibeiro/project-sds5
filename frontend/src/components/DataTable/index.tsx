bcd369a5935c5869998f104619b5fef3f638e2a1
Rodrigo Luis Ribeiro <rodrigo.ribeiro0909@gmail.com>
2021-11-02 11:11:37 -0300

    Basic Static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,88 @@
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>44</td>
+                        <td>38</td>
+                        <td>15017.00</td>
+                    </tr> <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Barns</td>
+                        <td>37</td>
+                        <td>33</td>
+                        <td>14005.00</td>
+                    </tr> <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Doodle</td>
+                        <td>34</td>
+                        <td>30</td>
+                        <td>13400.00</td>
+                    </tr> <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Hook</td>
+                        <td>33</td>
+                        <td>29</td>
+                        <td>13200.00</td>
+                    </tr> <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Jim</td>
+                        <td>32</td>
+                        <td>29</td>
+                        <td>13350.00</td>
+                    </tr> <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Karson</td>
+                        <td>30</td>
+                        <td>25</td>
+                        <td>12000.00</td>
+                    </tr> <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Larry</td>
+                        <td>29</td>
+                        <td>25</td>
+                        <td>12100.00</td>
+                    </tr> <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Mick</td>
+                        <td>29</td>
+                        <td>23</td>
+                        <td>11010.00</td>
+                    </tr> <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Owen</td>
+                        <td>27</td>
+                        <td>23</td>
+                        <td>11000.00</td>
+                    </tr> <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Paul</td>
+                        <td>27</td>
+                        <td>21</td>
+                        <td>10090.00</td>
+                    </tr> <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry White</td>
+                        <td>25</td>
+                        <td>20</td>
+                        <td>10150.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/rodrigoluisribeiro" target="_blank" rel="noreferrer">Rodrigo Luís Ribeiro</a></p>
+                <p className="text-light"> <a href="http://www.linkedin.com/in/rodrigo-ribeiro-238504129" target="_blank" rel="noreferrer">Linkedin</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
