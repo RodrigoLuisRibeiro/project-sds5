8d67bd12a2b3934e1db62ebeea266122feb9e6c1
Rodrigo Luis Ribeiro <rodrigo.ribeiro0909@gmail.com>
2021-11-05 12:02:55 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,29 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
 
 
 const Home = () => {
     return (
-        <div>
-           <h1>Página Home</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <br/>
+                    <h1 className="display-4">DSVendas</h1>
+                    <hr />
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <br/>
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                   <br/>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Acessar o dashboard
+                    </Link>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
