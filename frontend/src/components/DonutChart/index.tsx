679dccdabb22fc61a933abc8b5d1c1740f9e6d46
Rodrigo Luis Ribeiro <rodrigo.ribeiro0909@gmail.com>
2021-11-05 17:10:05 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { type } from 'os';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -11,23 +12,38 @@ type ChartData = {
 
 const DonutChart = () => {
 
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)      //concat
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+                // console.log(chartData);
+            });
+    }, []);
+
     //Forma errada
-    let chartData : ChartData = { labels: [], series: []};
+    // let chartData : ChartData = { labels: [], series: []};
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)      //concat
-         .then(response => {
-             const data = response.data as SaleSum[];
-             const myLabels = data.map(x => x.sellerName);
-             const mySeries = data.map(x => x.sum);
+    //Forma errada
+    //   axios.get(`${BASE_URL}/sales/amount-by-seller`)      //concat
+    //       .then(response => {
+    //          const data = response.data as SaleSum[];
+    //        const myLabels = data.map(x => x.sellerName);
+    //       const mySeries = data.map(x => x.sum);
 
-             chartData = { labels: myLabels, series: mySeries};
-             console.log(chartData);
-         });
+    //      chartData = { labels: myLabels, series: mySeries};
+    //     console.log(chartData);
+    //});
 
-  //  const mockData = {
-  //      series: [477138, 499928, 444867, 220426, 473088],
-  //      labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  //  }
+    //  const mockData = {
+    //      series: [477138, 499928, 444867, 220426, 473088],
+    //      labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //  }
 
     const options = {
         legend: {
