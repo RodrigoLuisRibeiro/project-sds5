336168f076fe56f53b20e23c8e0fc52728e6f2d2
Rodrigo Luis Ribeiro <rodrigo.ribeiro0909@gmail.com>
2021-11-05 23:34:56 -0300

    Env variable

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { type } from 'os';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
@@ -22,35 +21,14 @@ const DonutChart = () => {
                 const mySeries = data.map(x => x.sum);
 
                 setChartData({ labels: myLabels, series: mySeries });
-                // console.log(chartData);
             });
     }, []);
-
-    //Forma errada
-    // let chartData : ChartData = { labels: [], series: []};
-
-    //Forma errada
-    //   axios.get(`${BASE_URL}/sales/amount-by-seller`)      //concat
-    //       .then(response => {
-    //          const data = response.data as SaleSum[];
-    //        const myLabels = data.map(x => x.sellerName);
-    //       const mySeries = data.map(x => x.sum);
-
-    //      chartData = { labels: myLabels, series: mySeries};
-    //     console.log(chartData);
-    //});
-
-    //  const mockData = {
-    //      series: [477138, 499928, 444867, 220426, 473088],
-    //      labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //  }
 
     const options = {
         legend: {
             show: true
         }
     }
-
 
     return (
         <Chart
