0fb3a23e91aea2fbbb3feddadde3d667c3157891
Rodrigo Luis Ribeiro <rodrigo.ribeiro0909@gmail.com>
2021-11-05 23:01:28 -0300

    Pagination

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,24 +1,24 @@
 import { Seller } from "./seller"
 
 export type Sale = {
-            id: number,
-            visited: number,
-            deals: number,
-            amount: number,
-            date: string,
+            id: number;
+            visited: number;
+            deals: number;
+            amount: number;
+            date: string;
             seller: Seller;
 }
 
 export type SalePage = {
     content?: Sale[];
-    last: boolean ,
-    totalPages: number,
-    totalElements:number ,
-    size?: number,
-    number: number,
-    first: boolean,
-    numberOfElements?: number,
-    empty?: false
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
 }
 
 export type SaleSum = {
